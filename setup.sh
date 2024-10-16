4f4a2c5707e9949ca3f40197b95e2b78e5610525
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2024-10-16 03:36:27 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gcc clang git build-essential tree vim -y
+sudo apt install gcc clang git build-essential tree vim python3-venv htop curl -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+python3 -m venv ~/global
 
 #for monster laptop
 echo -e "deb https://deb.tuxedocomputers.com/ubuntu $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/tuxedocomputers.list
