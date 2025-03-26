ffd0b34116dc99279a6ba48bc87d2fa62179fee8
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2025-03-26 22:59:56 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo apt install gcc clang git build-essential tree vim python3-venv htop curl f
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+sudo snap install yazi --classic
+cargo install eza
+
 python3 -m venv ~/global
 
 #for monster laptop
@@ -14,3 +17,21 @@ cd /etc/apt/trusted.gpg.d/
 sudo wget https://deb.tuxedocomputers.com/0x54840598.pub.asc
 sudo apt update
 sudo apt install tuxedo-control-center
+
+
+################# .bashrc ####################
+
+alias ll='eza --git -alh -F --total-size'
+alias la='eza -A'
+alias l='eza -F -A'
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
+PS1='\[\033[01;34m\]\w\[\033[00m\]\n➡️ '
