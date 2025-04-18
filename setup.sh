d434516f9201eab5f0d0af613d74cb8fab99c16f
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2025-04-18 23:44:28 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,25 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gcc clang git build-essential tree vim python3-venv htop curl ffmpeg mediainfo tigervnc-viewer net-tools nvtop btop -y
+sudo apt install gcc clang git build-essential tree python3-venv htop curl ffmpeg mediainfo tigervnc-viewer net-tools nvtop btop -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 sudo snap install yazi --classic
+sudo snap install helix --classic
+sudo snap install blender --classic
+sudo snap install freecad
 sudo snap install drawio
 cargo install eza
+cargo install starship --locked
+cargo install bat --locked
+
+sudo add-apt-repository ppa:fish-shell/release-4
+sudo apt update
+sudo apt install fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install edc/bass
 
 python3 -m venv ~/global
 
@@ -18,21 +30,3 @@ cd /etc/apt/trusted.gpg.d/
 sudo wget https://deb.tuxedocomputers.com/0x54840598.pub.asc
 sudo apt update
 sudo apt install tuxedo-control-center
-
-
-################# .bashrc ####################
-
-alias ll='eza --git -alh -F --total-size'
-alias la='eza -A'
-alias l='eza -F -A'
-
-function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
-}
-
-PS1='\[\033[01;34m\]\w\[\033[00m\]\n\[\e[1;97;46m\]->\[\e[0m\] '
