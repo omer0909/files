c00ced03281ea88c50a2d1f3b96fb658ee070c01
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2025-04-23 23:54:13 +0300

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require "wezterm"
+
+local config = {}
+
+config.color_scheme = 'Solarized (light) (terminal.sexy)'
+config.enable_scroll_bar=true
+config.default_cursor_style = 'BlinkingBar'
+config.default_prog = { '/usr/bin/fish', '-l' }
+
+return config
