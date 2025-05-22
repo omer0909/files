72da33c6194cf8de314dcec503151b24da175095
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2025-05-22 22:00:19 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,5 +6,6 @@ config.color_scheme = 'Solarized (light) (terminal.sexy)'
 config.enable_scroll_bar=true
 config.default_cursor_style = 'BlinkingBar'
 config.default_prog = { '/usr/bin/fish', '-l' }
+config.warn_about_missing_glyphs = false
 
 return config
