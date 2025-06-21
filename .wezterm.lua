f949505c1abe1d9de733f272fa63de4a005ea610
Ömer Faruk Öz <71132301+omer0909@users.noreply.github.com>
2025-06-21 18:21:24 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,6 @@ config.enable_scroll_bar=true
 config.default_cursor_style = 'BlinkingBar'
 config.default_prog = { '/usr/bin/fish', '-l' }
 config.warn_about_missing_glyphs = false
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
